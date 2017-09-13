8469e7dd4921af4683052266094e0b280c2b048d
snayrouz <samuel.nayrouz@gmail.com>
2017-09-12 20:36:04 -0600

    Add dynamic category index path based on category name

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  has_many :item_categories
+  has_many :categories, through: :item_categories
+
   validates :title, presence: true, uniqueness: true
   validates :description, :price, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   resources :items, only:[:new, :create, :index]
+
+  get "/:category_name", to: 'categories#show'
 end
